65f9781ec4b9773c220d63ca5614615ac0f09855
Oluwaseun Abiola <oluabiolaseun@gmail.com>
2026-02-02 14:27:29 +0100

    fix: deployment fix

--- src/features/employee/employee.controller.ts
+++ src/features/employee/employee.controller.ts
@@ -36,7 +36,7 @@ export class EmployeeController {
     const search = req.user?.email;
 
     if (!search) {
-      throw ApiError.badRequest('User email address not provided' + JSON.stringify(req.user));
+      throw ApiError.badRequest('User email address not provided');
     }
     const employee = await EmployeeRepository.read({ rows: 1, page: 1, filters: { search } });
 
