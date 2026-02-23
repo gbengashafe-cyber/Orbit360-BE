693ae5f2fa150255c3b9ac73390bfdac95aad795
Zoe <zoezebedee1@gmail.com>
2026-02-23 17:51:13 +0100

    refactor: Remove multer middleware for file uploads from app configuration

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import multer from 'multer';
 import { randomUUID } from 'node:crypto';
 import path from 'path';
 import swaggerUi from 'swagger-ui-express';
@@ -104,19 +103,6 @@ app.use(
 );
 
 app.use(express.urlencoded({ extended: true }));
-
-// Configure multer for file  uploads
-const storage = multer.memoryStorage();
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 50 * 1024 * 1024, // This 50MB per file
-    files: 10, //this a max 10 files
-  },
-});
-
-// make mid-ware available globally
-app.use(upload.any());
 
 app.use(
   cors({
